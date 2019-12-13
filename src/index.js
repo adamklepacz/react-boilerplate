69b1b1e8c145ad8dbbec9f2167a4e80f667ea128
Adam Klepacz <adamklepacz92@gmail.com>
2019-12-13 18:18:09 +0100

    Add basic webpack babel and react configuration

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <div className="app">This is bdsadaasic test app.</div>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "public")
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    hot: true
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
