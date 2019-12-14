666b3a96a669718ba616c59d4f64d9c7218ca735
Adam Klepacz <adamklepacz92@gmail.com>
2019-12-14 10:30:36 +0100

    Test HMR

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,9 +3,8 @@ import './App.scss';
 
 const App = () => (
   <div className="app">
-    <span>test</span>
-test
-    <h2>dsada</h2>
+    <span>React Boilerplate</span>
+    <h2>Test</h2>
   </div>
 );
 
